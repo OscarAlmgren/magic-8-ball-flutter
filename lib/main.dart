85b692a8c96c5a7e44d625d555a23e6d9bb87fcf
Oscar Almgren <oscar.almgren@gmail.com>
2020-05-25 08:18:08 +0200

    Challenge done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        title: 'The Magic 8-ball',
+        home: BallPage(),
+        color: Colors.indigo,
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black45,
+        title: Text('Ask me anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballState = 1;
+
+  void _rollBall() {
+    setState(() {
+      ballState = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.indigo,
+      child: Center(
+        child: FlatButton(
+          onPressed: () => _rollBall(),
+          child: Image.asset('images/ball$ballState.png'),
+        ),
+      ),
+    );
+  }
+}
